83519c62b7e9724e428377e46cb1e805978b4468
praveenkumar19 <praveenkumar.a.cyber29@princedrkvasudevan.com>
2026-09-24 15:10:42 +0530

    Deploy project1 to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/WEB-INTERFACE-PROJECTS/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
