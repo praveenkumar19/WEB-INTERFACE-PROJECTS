74792ce8bb2d24f5ba5a736eb0f8e025f29e9692
praveenkumar19 <praveenkumar.a.cyber29@princedrkvasudevan.com>
2026-09-24 15:41:15 +0530

    Add Project 2 to GitHub Pages build

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/WEB-INTERFACE-PROJECTS/',
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        project2: 'project2/index.html'
+      }
+    }
+  }
 })
